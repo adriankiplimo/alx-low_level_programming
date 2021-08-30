91a64ed05fdf96b0d11900ebb2a573cfee56aebb
adriankiplimo <adriankiplimo@gmail.com>
2021-08-30 11:58:09 -0700

    Betty Updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 /* #!/bin/bash */
 #include <stdio.h>
-int main(void) /*This is the main function*/
+int main(void) /* This is the main function */
 {
 char Programming[] = "\"Programming is like building a multilingual puzzle";
 puts(Programming);
