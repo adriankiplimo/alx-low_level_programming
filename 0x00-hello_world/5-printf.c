f693953106176b31bceb957070ab7b7ad60b59b2
adriankiplimo <adriankiplimo@gmail.com>
2021-08-30 13:07:20 -0700

    Betty Updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - prints phrase "with proper grammar, but the outcome is a piece of art,"
+ * main - prints phrase with proper grammar, but the outcome is a piece of art,
  * Return: 0 if exit is successful, otherwise returns non-zero
 */
 int main(void)
