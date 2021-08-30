538d8c655e03ae4c87879cc99b33979fb1402e7e
adriankiplimo <adriankiplimo@gmail.com>
2021-08-30 11:50:50 -0700

    Betty Updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 int main()
 {
-char Programming[] = "\"Programming is like building a multilingual puzzle";
-  
+char Programming[] = "\"Programming is like building a multilingual puzzle";  
 puts(Programming);
 
- return (0);
+return (0);
 }
