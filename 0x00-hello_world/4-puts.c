ff971a779355aefe15086182132b8f208841f7ef
adriankiplimo <adriankiplimo@gmail.com>
2021-08-30 13:02:35 -0700

    Betty Updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main prints the phrase "Programming is like building a multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * using puts, returns a 0 if successful, otherwise returns non-zero
 */
 int main(void)
