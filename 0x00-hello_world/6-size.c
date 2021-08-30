399d6c33ae6726b8aede60c4b6c95dc25857e5ec
adriankiplimo <adriankiplimo@gmail.com>
2021-08-30 12:17:45 -0700

    Betty Updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,22 @@
-/* #!/bin/bash */
 #include<stdio.h>
-int main()
+/**
+   * main - prints the size of various types
+   * Return: 0 if exited properly, non-zero otherwise
+   */
+int main(void)
 {
-  char charType;
-  int intType;
-  long int longIntType;
-  long long int longLongIntType;
-  float floatType;
+char charType;
+int intType;
+long int longIntType;
+long long int longLongIntType;
+float floatType;
 
-  // sizeof evaluates the size of a variable
-    printf("Size of char: %d byte(s)\n", sizeof(charType));
-    printf("Size of int: %d byte(s)\n", sizeof(intType));
-    printf("Size of long int: %d byte(s)\n", sizeof(longIntType));
-    printf("Size of long long int: %d byte(s)\n", sizeof(longLongIntType));
-    printf("Size of float: %d byte(s)\n", sizeof(floatType));
+/* sizeof evaluates the size of a variable */
+printf("Size of char: %d byte(s)\n", sizeof(charType));
+printf("Size of int: %d byte(s)\n", sizeof(intType));
+printf("Size of long int: %d byte(s)\n", sizeof(longIntType));
+printf("Size of long long int: %d byte(s)\n", sizeof(longLongIntType));
+printf("Size of float: %d byte(s)\n", sizeof(floatType));
     
-    return 0;
+return (0);
 }
