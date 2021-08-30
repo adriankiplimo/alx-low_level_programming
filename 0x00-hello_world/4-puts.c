ae5e8bbfb0d88623cb225c1e34ef74a6ade3a8f7
adriankiplimo <adriankiplimo@gmail.com>
2021-08-30 12:13:18 -0700

    Betty Updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main prints the phrase "Programming is like building a multilingual puzzle
  * using puts, returns a 0 if successful, otherwise returns non-zero
 */
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*
+/**
  * main prints phrase "with proper grammar, but the outcome is a piece of art,"
  * returns 0 if exit is successful, otherwise returns non-zero
 */
