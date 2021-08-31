6edbf269d487ae32caa845e402967de6d378502b
adriankiplimo <adriankiplimo@gmail.com>
2021-08-31 02:46:04 -0700

    Betty update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,24 +3,24 @@
 /**
  * main-prints all the numbers of base 16 in lowercase,
  * followed by a new line
- * Returns:0 if successful
+ * Return:0 if successful
  * otherwise returns non-zero
 */
 
 int main(void)
 {
-  int num;
-  char ch;
+int num;
+char ch;
 
-  for(num = 48; num < 58; num++)
-    {
-      putchar(num);
-    }
-  for(ch = 'a'; ch <= 'f'; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
+for (num = 48; num < 58; num++)
+{
+putchar(num);
+}
+for(ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
 
 return (0);
 }
