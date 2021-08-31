008c712ab3ed22e66591d0a2a832e3a851636ebe
adriankiplimo <adriankiplimo@gmail.com>
2021-08-31 02:38:27 -0700

    betty update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
 /**
  * main-prints base10 numbers from 0-9,
  * only using the putchar function
- * Returns:0 if successful
+ * Return:0 if successful
  * otherwise returns non-zero
 */
 
 int main(void)
 {
 int num = 48;
-while(num < 58)
+while (num < 58)
 {
 putchar(num);
 num++;
