3d3f4d6d817cdfa0f481b148050862967a18b07a
adriankiplimo <adriankiplimo@gmail.com>
2021-09-04 04:02:15 -0700

    Correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,17 +7,17 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
-printf("%s ","Fizz");
+printf("%s ","FizzBuzz");
 }
 else if (i % 5 == 0)
 {
 printf("%s ","Buzz");
 }
-else if (i % 3 ==0 && i % 5 == 0)
+else if (i % 3 ==0)
 {
-printf("%s ","FizzBuzz");
+printf("%s ","Fizz");
 }
 else
 {
