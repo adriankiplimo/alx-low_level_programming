66dfe3ee5112a28398bd9ec84fe05fac50e366c1
abukiplimo <adriankiplimo@gmail.com>
2021-09-07 12:07:46 -0700

    Betty Updates

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,22 +10,21 @@ void rev_string(char *s)
 int length = 0, i, j;
 char *str, temp;
 
- while (length >= 0)
-   {
-     if (s[length] == '\0')
-       break;
-     length++;
-   }
- str = s;
+while (length >= 0)
+{
+if (s[length] == '\0')
+break;
+length++;
+}
+str = s;
 
- for (i = 0; i < (length - 1); i++)
-   {
-     for (j = i + 1; j > 0; j--)
-       {
-	 temp = *(str + j);
-	 *(str + j) = *(str + (j - 1));
-	 *(str + (j - 1)) = temp;
-       }
-   }
-  
+for (i = 0; i < (length - 1); i++)
+{
+for (j = i + 1; j > 0; j--)
+{
+temp = *(str + j);
+*(str + j) = *(str + (j - 1));
+*(str + (j - 1)) = temp;
+}
+}
 }
