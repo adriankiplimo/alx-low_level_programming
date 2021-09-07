52f329359e64d316458e6c36e58e584f55e9909f
abukiplimo <adriankiplimo@gmail.com>
2021-09-07 12:13:48 -0700

    Betty updates

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* *_strcpy - copies the string pointed to by src, 
+* *_strcpy - copies the string pointed to by src,
 * including the terminating null byte (\0), to the buffer pointed to by dest.
 * @dest: character pointer variable
 * @src: character pointer variable
@@ -18,5 +18,5 @@ while (*(src + count))
 count++;
 }
 *(dest + count) = '\0';
-return(dest);
+return (dest);
 }
