13e0a941de9748d7007579f9c30efd3d81005808
abukiplimo <adriankiplimo@gmail.com>
2021-09-10 02:33:42 -0700

    Betty updates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,11 @@ char *_strcat(char *dest, char *src)
 {
 int i, j = 0;
 
-for (i = 0; dest[i] != '\0'; i++)
+for (i = 0;dest[i] != '\0'; i++)
 ;
 while (src[j] != '\0')
 {
-dest[i+j] = src[j];
+dest[i + j] = src[j];
 j++;
 }
 
