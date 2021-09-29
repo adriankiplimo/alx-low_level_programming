fb911dec437ab78296be7559516ec3223a422214
abukiplimo <adriankiplimo@gmail.com>
2021-09-29 09:23:50 -0700

    header update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,11 @@
-/* FUNC_POINTERS_H */
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif /*FUNC_POINTERS*/
