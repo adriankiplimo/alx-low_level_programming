41af7dae9bf8c77c242ce4710789208294aea992
abukiplimo <adriankiplimo@gmail.com>
2021-10-05 05:31:38 -0700

    betty updates

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,8 +17,8 @@
 */
 typedef struct printer
 {
-  char *symbol;
-  void (*print)(va_list arg);
+char *symbol;
+void (*print)(va_list arg);
 } printer_t;
 
 int sum_them_all(const unsigned int n, ...);
