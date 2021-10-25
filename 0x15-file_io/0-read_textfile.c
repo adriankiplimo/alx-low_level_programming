c6fc5768b4de7ed140afb91bf7f9aef2a9e38a80
abukiplimo <adriankiplimo@gmail.com>
2021-10-24 23:12:46 -0700

    Betty Updates

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,30 +12,30 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  ssize_t op, rd, wr;
-  char *buff;
+ssize_t op, rd, wr;
+char *buff;
 
-  if (filename == NULL)
-    return(0);
+if (filename == NULL)
+return(0);
 
-  buff = malloc(sizeof(char) * letters);
+buff = malloc(sizeof(char) * letters);
 
-  if (buff == NULL)
-    return(0);
+if (buff == NULL)
+return(0);
 
-  op = open(filename, O_RDONLY);
-  rd = read(op, buff, letters);
-  wr = write(STDOUT_FILENO, buff, rd);
+op = open(filename, O_RDONLY);
+rd = read(op, buff, letters);
+wr = write(STDOUT_FILENO, buff, rd);
 
-  if (op == -1 || rd == -1 || wr == -1 || wr != rd)
-    {
-      free(buff);
-      return(0);
-    }
+if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+{
+free(buff);
+return(0);
+}
 
-  free(buff);
-  close(op);
+free(buff);
+close(op);
 
-  return(wr);
+return(wr);
 
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,25 +10,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-  int op, wr, len = 0;
-  
-  if (filename == NULL)
-    return(-1);
+int op, wr, len = 0;
 
-  if (text_content == NULL)
-    {
-      for(len = 0; text_content[len];)
-	len ++;
-    }
+if (filename == NULL)
+return(-1);
 
-  op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-  wr = write(op, text_content, len);
+if (text_content == NULL)
+{
+for(len = 0; text_content[len];)
+len ++;
+}
 
-  if(op == -1 || wr == -1)
-    return(-1);
+op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+wr = write(op, text_content, len);
 
-  close(op);
+if(op == -1 || wr == -1)
+return(-1);
 
-  return(1);
+close(op);
+
+return(1);
 
 }
