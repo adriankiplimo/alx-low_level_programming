f37d3302e54402fcea21a3a4d993489bedaf8952
akakiplimo <adriankiplimo@gmail.com>
2021-12-06 10:17:36 +0000

    kernel style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: HEAD of doubly linked list
- *
- * Return: number of nodes
- */
+* print_dlistint - prints all the elements of a dlistint_t list
+* @h: HEAD of doubly linked list
+*
+* Return: number of nodes
+*
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t nodes = 0;
