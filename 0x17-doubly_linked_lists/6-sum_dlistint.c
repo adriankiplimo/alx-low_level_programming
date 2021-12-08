0974b3b6ca3710b312a52b1c8ae3fa5c884b66b7
akakiplimo <adriankiplimo@gmail.com>
2021-12-08 11:41:10 +0000

    kernel style code

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
-* sum_dlistint - calculates sum of all the data of a dlistint_t linked list
+* sum_dlistint - calculates sum of all the data
 * @head: pointer to the HEAD of the dlist
+*
 * Return: sum of all the data (n), if list is empty return 0
 */
 int sum_dlistint(dlistint_t *head)
