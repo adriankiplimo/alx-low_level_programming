b0ac9499c1908ca2c9e6c54d72b0246d741cca0e
A. Kiplimo <adriankiplimo@gmail.com>
2022-09-01 21:17:02 +0300

    Delete 0x02-functions_nested_loops directory

--- 0x02-functions_nested_loops/6-abs.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-/**
- * _abs - computes the absolute value of an integer
-*/
-int _abs(int n)
-{
-return abs(n);
-}
--- 0x02-functions_nested_loops/README.md
+++
@@ -1,2 +0,0 @@
-# 0x02-functions_nested_loops
-This is a repo about functions and nested loops predominantly in C programming language
