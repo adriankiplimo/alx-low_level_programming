deb5556ee3463c5b7034cf5fb5bccb068c977e13
akakiplimo <akakiplimo@gmail.com>
2022-09-03 04:15:36 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
