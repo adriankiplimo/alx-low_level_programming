bc83787d8d2893d1ce5bcf0d014b049e8d4ca579
akakiplimo <akakiplimo@gmail.com>
2022-10-06 23:16:57 -0700

    Interpolation prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,6 @@ void print_array(int *array, int left, int right);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
