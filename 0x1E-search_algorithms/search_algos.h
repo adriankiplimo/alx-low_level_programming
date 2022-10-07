0f8273535e4f32720e055d48ce965ad281f7c538
akakiplimo <akakiplimo@gmail.com>
2022-10-06 22:59:38 -0700

    Jump search prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ void print_array(int *array, int left, int right);
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
