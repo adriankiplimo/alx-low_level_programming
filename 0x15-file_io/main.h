fb9656a0ceaa232dc7eb0845c661e5d2ea1ce56f
akakiplimo <akakiplimo@gmail.com>
2022-12-08 05:54:18 -0800

    add utility functions

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,24 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/**
+ *  _strlen - length of a string
+ *  @s: input char
+ *  Return: length of a string
+ *       
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+i++;
+}
+
+return (i);
+}
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
