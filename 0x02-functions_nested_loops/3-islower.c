bd0fede7e407c87add4389094e6caab075594b29
akakiplimo <akakiplimo@gmail.com>
2022-12-08 04:46:56 -0800

    fix betty errors

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 
 /**
  * _islower - checks for lowercase character
- * Return: 1 if lowercase
- * Return: 0 otherwise
+ * @c: integer to be checked
+ * Return: 1 if lowercase, 0 otherwise
 */
 int _islower(int c)
 {
-  if (islower(c))
-    return (1);
-  return (0);
+if (islower(c))
+return (1);
+return (0);
 }
