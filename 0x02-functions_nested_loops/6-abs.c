dd65cbe711b8a90d99dc4f5166d19ec10c262f76
akakiplimo <akakiplimo@gmail.com>
2022-12-08 04:55:38 -0800

    betty fix abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,10 @@
 
 /**
  * _abs - computes the absolute value of an integer
+ * @n: integer to be used
+ * Return: The absolute value of an int
 */
 int _abs(int n)
 {
-return abs(n);
+return (abs(n));
 }
