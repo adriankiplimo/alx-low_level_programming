d0c8de70b8482da7673f4629d26779099c612759
akakiplimo <akakiplimo@gmail.com>
2022-12-08 04:58:04 -0800

    betty errors fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 
 /**
- * print_last_digit - prints the last digit of a number.
- * return: value of the last digit
+ * print_last_digit - prints the last digit of a number
+ * @n: input integer
+ * Return: value of the last digit
 */
 int print_last_digit(int n)
 {
 int num;
-  
+
 if (n < 0)
 num = -1 * (n % 10);
 else
 num = n % 10;
 
 _putchar(48 + num);
-  
+
 return (num);
 }
